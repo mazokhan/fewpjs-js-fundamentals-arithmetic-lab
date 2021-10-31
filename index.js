04df940b79a878e7109d548f283ec13685219074
mazen <mak9986@hotmail.com>
2021-10-31 21:03:00 +1100

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const mulitply = (num1 * num2 2);
+
+const random = Math.floor(math.random() * 10) + 1
+
+const num3 = 8;
+const num4 = 2;
+
+const mod = (num3 % num4);
+
+const max = Math.max(1,8,10,20);
